1bc4d03cd4987cd36c82f3fb32504e735e894ab8
Grosbug <grosbug06@yahoo.fr>
2026-05-28 18:02:24 +0200

    feat(backend): register WorkflowsModule + NodeTemplatesModule; drop stub AppController

--- backend/src/app.module.ts
+++ backend/src/app.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common'
-import { AppController } from './app.controller'
-import { AppService } from './app.service'
 import { PrismaModule } from './prisma/prisma.module'
+import { WorkflowsModule } from './workflows/workflows.module'
+import { NodeTemplatesModule } from './node-templates/node-templates.module'
 
 @Module({
-  imports: [PrismaModule],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [PrismaModule, WorkflowsModule, NodeTemplatesModule]
 })
 export class AppModule {}
